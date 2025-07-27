f568c926c26dc57e8d1280d586a78da98c331417
Wai Yip <knyl2013@gmail.com>
2025-07-27 18:53:45 -0400

    Fix

--- all_dockerless.sh
+++ all_dockerless.sh
@@ -5,7 +5,7 @@ export HF_TOKEN=
 curl -LsSf https://astral.sh/uv/install.sh | sh
 curl https://sh.rustup.rs -sSf | sh
 curl -fsSL https://get.pnpm.io/install.sh | sh -
-source ~/.bashrc
+eval "$(cat ~/.bashrc | tail -n +10)"
 apt-get update
 apt-get install -y vim libssl-dev pkg-config cmake
 ./dockerless/start_frontend.sh &
