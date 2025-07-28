c85e022b426be2b577ffb9353c6282aaa838089c
Wai Yip <knyl2013@gmail.com>
2025-07-28 00:10:03 -0400

    Update model

--- dockerless/start_llm.sh
+++ dockerless/start_llm.sh
@@ -3,7 +3,7 @@ set -ex
 cd "$(dirname "$0")/.."
 
 uv tool run vllm serve \
-  --model=google/gemma-3-4b-it \
+  --model=google/gemma-3-12b-it \
   --max-model-len=8192 \
   --dtype=bfloat16 \
   --gpu-memory-utilization=0.3 \
