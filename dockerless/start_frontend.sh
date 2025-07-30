1b9ebee1657649bdca94afd436ed3f2c0e993e01
Wai Yip <knyl2013@gmail.com>
2025-07-29 23:23:32 -0400

    Add NVM installation and environment setup to start_frontend.sh

--- dockerless/start_frontend.sh
+++ dockerless/start_frontend.sh
@@ -3,6 +3,8 @@ set -ex
 cd "$(dirname "$0")/.."
 
 cd frontend-new
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+eval "$(cat ~/.bashrc | tail -n +10)"
 nvm install 20.19.0 
 nvm use 20.19.0
 pnpm install
