547196ba2961af07e9bc4c69617e1e93ddacf2ce
Wai Yip <knyl2013@gmail.com>
2025-08-03 23:20:42 -0400

    Debug

--- frontend-new/src/lib/components/PhoneCallScreen.svelte
+++ frontend-new/src/lib/components/PhoneCallScreen.svelte
@@ -122,7 +122,7 @@
   }
 
   $: {
-    const webSocketUrl = "ws://213.173.108.103:8000/v1/realtime";
+    const webSocketUrl = "wss://213.173.108.103:8000/v1/realtime";
 
     if (shouldConnect && !ws) {
       console.log("Connecting to WebSocket...");
