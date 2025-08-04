a8c939a32335e194c78611c6b406abc08c1a811e
Wai Yip <knyl2013@gmail.com>
2025-08-03 21:10:57 -0400

    Fix bash

--- all_dockerless.sh
+++ all_dockerless.sh
@@ -19,7 +19,7 @@ wait_for_port() {
 run_initial_setup() {
   echo "--- Running one-time initial setup ---"
   curl -LsSf https://astral.sh/uv/install.sh | sh
-  curl https://sh.rustup.rs -sSf | sh
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
   curl -fsSL https://get.pnpm.io/install.sh | sh -
 
   eval "$(cat ~/.bashrc | tail -n +10)"
