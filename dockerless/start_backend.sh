427174ebf94995432442590ecc641c632cca23f4
Wai Yip <knyl2013@gmail.com>
2025-08-05 21:46:50 -0400

    Add `no-dev`

--- dockerless/start_backend.sh
+++ dockerless/start_backend.sh
@@ -2,4 +2,4 @@
 set -ex
 cd "$(dirname "$0")/.."
 
-uv run uvicorn unmute.main_websocket:app --reload --host 0.0.0.0 --port 8000 --ws-per-message-deflate=false
+uv run --no-dev uvicorn unmute.main_websocket:app --reload --host 0.0.0.0 --port 8000 --ws-per-message-deflate=false
