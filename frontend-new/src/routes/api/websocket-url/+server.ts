04fe68f9f25a0f1ed3cd1f49f61066f5f21ed098
Wai Yip <knyl2013@gmail.com>
2025-08-07 22:40:49 -0400

    Change ws server

--- frontend-new/src/routes/api/websocket-url/+server.ts
+++ frontend-new/src/routes/api/websocket-url/+server.ts
@@ -8,7 +8,7 @@ import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async () => {
   // In the future, you could have dynamic logic here to select a server,
   // generate a temporary token, etc.
-  const webSocketUrl = "wss://hanr03diihycu5-8000.proxy.runpod.net/v1/realtime";
+  const webSocketUrl = "wss://utmbqrhqfi6ay0-8000.proxy.runpod.net/v1/realtime";
 
   // We return the URL in a JSON object.
   // The `json()` helper from SvelteKit sets the correct Content-Type header.
