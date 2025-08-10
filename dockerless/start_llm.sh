90911f69cc3ac223db89ce2adff5b8eb912ae140
Wai Yip <knyl2013@gmail.com>
2025-08-09 21:44:56 -0400

    Test different model

--- dockerless/start_llm.sh
+++ dockerless/start_llm.sh
@@ -3,7 +3,7 @@ set -ex
 cd "$(dirname "$0")/.."
 
 uv tool run vllm serve \
-  --model=google/gemma-3n-e4b \
+  --model=google/gemma-3n-e2b-it \
   --max-model-len=8192 \
   --dtype=bfloat16 \
   --gpu-memory-utilization=0.3 \
