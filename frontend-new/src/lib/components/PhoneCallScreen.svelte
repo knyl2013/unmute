4c7282556bc4db24711a1335d84eb6b2a16747c9
Wai Yip <knyl2013@gmail.com>
2025-08-13 19:51:06 -0400

    Fix mobile screen

--- frontend-new/src/lib/components/PhoneCallScreen.svelte
+++ frontend-new/src/lib/components/PhoneCallScreen.svelte
@@ -351,11 +351,10 @@
   }
 
   .callContainer {
-    width: 100vw;
-    height: 100vh;
-    height: 100dvh; /* Fix for mobile screens */
+    position: fixed;
+    width: 100%;
+    height: 100%;
     overflow: hidden;
-    position: relative;
     display: flex;
     flex-direction: column;
     color: white;
